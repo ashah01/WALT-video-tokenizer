91c32d614054fcb55f7c1c110a15df5e50549ec3
Arnav Shah <shaharnav03@gmail.com>
2024-08-16 02:18:21 +0000

    god level results

--- eval.py
+++ eval.py
@@ -4,7 +4,7 @@ from magvit2_pytorch import VideoTokenizer, VideoTokenizerTrainer
 from condition_dataset import ConditionalVideoDataset
 
 dataset = ConditionalVideoDataset("frames_dataset")
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
 x = next(iter(dataloader)).to('cuda')
 
 # torch.cuda.get_mem_info() is your friend in combating nasty OOMs
@@ -12,16 +12,29 @@ x = next(iter(dataloader)).to('cuda')
 
 tokenizer = VideoTokenizer(
     image_size=128,
-    init_dim=22,
+    init_dim=28,
     num_res_blocks=1,
-    ch_mult=(1, 2),
+    ch_mult=(2,),
     z_channels=256,
     perceptual_loss_weight=0,
     use_gan=False,
     adversarial_loss_weight=0,
 ).to('cuda')
 
-tokenizer.load("checkpoints/checkpoint.pt")
+# tokenizer.load("new_checkpoints/checkpoint.pt")
+checkpoint = torch.load("new_checkpoints/checkpoint.pt")
+
+# Remove 'module.' from parameter names
+state_dict = checkpoint['model']
+new_state_dict = {}
+
+for k, v in state_dict.items():
+    # Remove 'module.' from the key
+    name = k.replace("module.", "")
+    new_state_dict[name] = v
+
+# Load the updated state dict
+tokenizer.load_state_dict(new_state_dict, strict=True)
 tokenizer.eval()
 
 with torch.no_grad():
